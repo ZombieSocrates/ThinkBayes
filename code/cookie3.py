531f12d21dba92ec04e4d08f4d6c3e14ca286ce4
Ross Weijer <ross.weijer@datascopeanalytics.com>
2017-08-26 12:26:27 -0500

    Light refactor/edge cases for cookie3.py

--- code/cookie3.py
+++ code/cookie3.py
@@ -8,28 +8,53 @@ License: GNU GPLv3 http://www.gnu.org/licenses/gpl.html
 from thinkbayes import Pmf
 
 class Bowl(object):
+    '''A separate object that keeps track of how many
+    cookies of each type are in a bowl. All it basically
+    does is produce likelihoods and update those upon
+    selection
+    '''
     def __init__(self, n_vanilla, n_chocolate):
         self.vanilla = n_vanilla
         self.chocolate = n_chocolate
 
     def get_bowl_likelihood(self):
+        '''Added an edge case to account for the possibility of
+        someone trying to select more than V + C cookies from
+        the bowl
+        '''
         total = self.vanilla + self.chocolate
+        if total == 0:
+            return dict(vanilla=0.0, chocolate = 0.0)
         v_prob = float(self.vanilla * 1.0/total)
         c_prob = float(self.chocolate * 1.0/total)
         return dict(vanilla=v_prob, chocolate = c_prob)
 
     def update_bowl_likelihood(self, cookie_string):
+        '''Edge case added to avoid the possibility of
+        negative cookies 
+        '''
         if cookie_string not in ['vanilla','chocolate']:
             raise NotImplementedError
         elif cookie_string == 'vanilla':
-            self.vanilla += -1
+            self.vanilla = max(self.vanilla - 1, 0)
         else:
-            self.chocolate += -1
+            self.chocolate = max(self.chocolate -1, 0)
         return self.get_bowl_likelihood()
 
 
 class Cookie(Pmf):
     """A map from string bowl ID to probablity."""
+
+    # Pmf starts out with two bowls of 40 cookies each,
+    # and populates the mixes dictionary with the initial
+    # bowl likelihoods
+    B1 = Bowl(n_vanilla=30, n_chocolate=10)
+    B2 = Bowl(n_vanilla=20, n_chocolate=20)
+
+    mixes = {
+        'Bowl 1':B1.get_bowl_likelihood(),
+        'Bowl 2':B2.get_bowl_likelihood(),
+        }
 
     def __init__(self, hypos):
         """Initialize self.
@@ -51,16 +76,6 @@ class Cookie(Pmf):
             self.Mult(hypo, like)
         self.Normalize()
 
-    # replace the inner dictionaries of this with the
-    # bowl likelihood dictionaries
-    B1 = Bowl(n_vanilla=30, n_chocolate=10)
-    B2 = Bowl(n_vanilla=20, n_chocolate=20)
-
-    mixes = {
-        'Bowl 1':B1.get_bowl_likelihood(),
-        'Bowl 2':B2.get_bowl_likelihood(),
-        }
-
     def Likelihood(self, data, hypo):
         """The likelihood of the data under the hypothesis.
 
@@ -80,13 +95,14 @@ def main():
     dataset = ['vanilla','chocolate','vanilla']
     replacement = True
     for data in dataset:
-        print 'Bowl 1: Chocolate {}, Vanilla {}'.format(pmf.B1.chocolate, pmf.B1.vanilla)
-        print 'Bowl 2: Chocolate {}, Vanilla {}'.format(pmf.B2.chocolate, pmf.B2.vanilla)
+        # print 'Bowl 1: Chocolate {}, Vanilla {}'.format(pmf.B1.chocolate, pmf.B1.vanilla)
+        # print 'Bowl 2: Chocolate {}, Vanilla {}'.format(pmf.B2.chocolate, pmf.B2.vanilla)
         pmf.Update(data)
+        # If sampling with replacement, update the mixes dictionary after each draw
         if replacement:
             pmf.mixes['Bowl 1'] = pmf.B1.update_bowl_likelihood(data)
             pmf.mixes['Bowl 2'] = pmf.B2.update_bowl_likelihood(data)
-            print 'removed {}\n'.format(data)
+            # print 'removed {}\n'.format(data)
 
 
     for hypo, prob in pmf.Items():
