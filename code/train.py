82ba25c269aca85590d86e31f52718238cf1dee7
Ross Weijer <ross.weijer@datascopeanalytics.com>
2017-08-26 13:23:06 -0500

    comment out plotting and add uniform prior convergence example to train.py

--- code/train.py
+++ code/train.py
@@ -18,18 +18,23 @@ class Train(Dice):
 
 
 def main():
-    hypos = xrange(1, 1001)
-    suite = Train(hypos)
+    uppers = [500,1000,2000]
+    for upper in uppers:
+        hypos = xrange(1, upper + 1)
+        suite = Train(hypos)
 
-    suite.Update(60)
-    print suite.Mean()
+        data_vals = [60,30,90]
+        for i, data in enumerate(data_vals):
+            suite.Update(data)
+            print "Prior from 1 to {}, evidence {}".format(upper, data_vals[:i+1])
+            print "Mean of posterior: {}".format(suite.Mean())
 
-    thinkplot.PrePlot(1)
-    thinkplot.Pmf(suite)
-    thinkplot.Save(root='train1',
-                   xlabel='Number of trains',
-                   ylabel='Probability',
-                   formats=['pdf', 'eps'])
+    # thinkplot.PrePlot(1)
+    # thinkplot.Pmf(suite)
+    # thinkplot.Save(root='train1',
+    #                xlabel='Number of trains',
+    #                ylabel='Probability',
+    #                formats=['pdf', 'eps'])
 
 
 if __name__ == '__main__':
