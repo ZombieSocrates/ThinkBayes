c7a64a33bc19d762700dcb98a5527e091d1ec599
Ross Weijer <ross@datascopeanalytics.com>
2019-01-10 09:03:42 -0600

    initialization and get_quality/get_reliability methods sketched out in Chap4 Exercise 2

--- code/04chapter_exercise2.py
+++ code/04chapter_exercise2.py
@@ -1,7 +1,10 @@
-import thinkbayes
-import thinkplot
+# import thinkbayes
+# import thinkplot
 
 import ipdb
+
+from random import uniform
+from thinkbayes import Beta
 
 '''
 Redditors post links to online content and other web pages. Other redditors 
@@ -23,15 +26,11 @@ updates both objects whenever a redditor casts a vote.
 
 MY ANSWER: IN PROGRESS
 
-IDEA SO FAR:
-
-Model both users and links as beta distributions.
-
-At least in the case of links, the parameters alpha and beta
-could correspond rather nicely to number of upvotes and number of downvotes
-
-For users, you could have something similar with "correct votes" vs
-"incorrect votes."
+Model both users and links as beta distributions. I originally thought that in 
+the case of links, the parameters alpha and beta could correspond rather 
+nicely to number of upvotes and number of downvotes For users, you could have 
+something similar with "correct votes" vs "incorrect votes." HOWEVER, the whole 
+gist here is that each vote shouldn't have an equal impact..
 
 
 I had this half-formed idea of being able to update each object by 
@@ -51,6 +50,8 @@ and I need to think about how a score would ideally change if
 	* A highly reliable user incorrectly votes
 	* A highly unreliable user incorrectly votes
 
+
+
 '''       
 
 
@@ -58,26 +59,59 @@ and I need to think about how a score would ideally change if
 
 class RedditUser():
 
-	def __init__(self):
-		pass
+	def __init__(self, good_karma = 1, bad_karma = 1):
+		'''Might not be a way around this, but every time update is called
+		we would need to reset the good_karma and bad_karma attribute. This
+		should be handled in the `update_from_vote` method.
+		'''
+		if good_karma * bad_karma == 0:
+			raise ValueError("Neither user parameter can be set to zero")
+		self.reliability_dist = thinkbayes.Beta(good_karma, bad_karma)
+		self.good_karma = self.reliability_dist.alpha
+		self.bad_karma = self.reliability_dist.beta
 
-	def update(self):
+	def get_user_reliability(self):
+		'''The estimated user reliability would be the mean of the beta 
+		distribution represented by its good_karma and bad_karma
+		'''
+		return self.reliability_dist.Mean()
+
+
+	def make_vote(self, reddit_link, vote_type):
+		'''What link is this person voting on and is it an upvote or a down 
+		vote? Still need to think about how the respective updates will happen.
+		'''
 		pass
 
 
 class RedditLink():
 
-	def __init__(self):
-		self.quality_dist = thinkbayes.Beta()
-		self.upvotes = self.quality_dist.alpha
-		self.downvotes = self.quality_dist.beta
+	def __init__(self, plus_score = 1, spam_score = 1):
+		'''Might not be a way around this, but every time update is called
+		we would need to reset the plus_score and spam_score attribute. This
+		should be handled in the `update_from_vote` method.
+		'''
+		if plus_score * spam_score == 0:
+			raise ValueError("Neither quality parameter can be set to zero")
+		self.quality_dist = Beta(plus_score, spam_score)
+		self.plus_score = self.quality_dist.alpha
+		self.spam_score = self.quality_dist.beta
+
+	def get_link_quality(self):
+		'''The estimated link quality would be the mean of the beta 
+		distribution represented by its plus_score and spam_score.
+		'''
+		return self.quality_dist.Mean()
+
+	def update_from_vote(self, reddit_user, vote_type):
+		'''What user is voting on this, and is it an upvote or a down vote? 
+		Still need to think about how the respective updates will happen.
+		'''
+		pass
 
 
 if __name__ == "__main__":
 	a_link = RedditLink()
-	# At this point, you need to modify the
-	# quality dist attribute to simulate the act
-	# of upvoting and downvoting
 	ipdb.set_trace()
 
 
